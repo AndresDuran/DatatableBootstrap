526a9046399b5ff89e0f258ffe725e09d32cee1d
AndresDuran <polux_@outlook.com>
2021-09-12 12:09:56 -0500

    Se corrigen datos finales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DatatableBootstrap';
+
   data: Array<any>;
 
   constructor() {
     this.data = [
-      { position: 20, name: 'calsio', weight: 40.078, symbol: 'ca' },
-      { position: 21, name: 'escandio', weight: 44.955, symbol: 'sc' },
-      { position: 22, name: 'titanio', weight: 47.867, symbol: 'ti' },
-      { position: 23, name: 'vanadio', weight: 50.9415, symbol: 'v' },
-      { position: 24, name: 'cromo', weight: 51.9961, symbol: 'cr' },
+      {cedula: 1070983062, nombre: 'Jorge Andres', apellidos: 'Duran Perez'},
+      {cedula: 1018488967, nombre: 'Ingry Rossana', apellidos: 'Bastidas Alfonso'},
 
     ]
   }
