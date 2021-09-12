0ee2a53dc2ec6aaedb77737886e72325b1d3793f
AndresDuran <polux_@outlook.com>
2021-09-12 11:59:16 -0500

    Se agrega la tabla con informacion desde el .ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DatatableBootstrap';
+  data: Array<any>;
+
+  constructor() {
+    this.data = [
+      { position: 20, name: 'calsio', weight: 40.078, symbol: 'ca' },
+      { position: 21, name: 'escandio', weight: 44.955, symbol: 'sc' },
+      { position: 22, name: 'titanio', weight: 47.867, symbol: 'ti' },
+      { position: 23, name: 'vanadio', weight: 50.9415, symbol: 'v' },
+      { position: 24, name: 'cromo', weight: 51.9961, symbol: 'cr' },
+
+    ]
+  }
 }
+
+
